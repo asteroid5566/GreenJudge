35a247e1eca8ff6504fd053646c88c63e4fbc127
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2021-03-02 00:30:01 +0800

    solution

---
+++ a002.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is a book." << endl;
+	cout << "That is a pen." << endl;
+	cout << "I am a student." << endl;
+	return 0;
+}
